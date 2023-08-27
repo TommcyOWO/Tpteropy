e4763251a44d9892e00134dc3e1aaa94bc83cb45
Tomycat#0573 <102529512+TommcyOWO@users.noreply.github.com>
2023-08-27 13:50:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ with open("README.md", "r",encoding="utf-8") as f:
     long_description = f.read()
     
 setuptools.setup(
-    name = "pteropy",
-    version = "0.1.7",
-    author = "Hans",
-    author_email="ccoccc14@gmail.com",
+    name = "Tpteropy",
+    version = "0.0.1",
+    author = "Tomycat",
+    author_email="tangminghong.tom@gmail.com",
     description="簡單存取Pterodactyl api",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/HansHans135/pteropy",                                         
+    url="https://github.com/TommcyOWO/Tpteropy",                                         
     packages=setuptools.find_packages(),     
     classifiers=[
         "Development Status :: 1 - Planning",
